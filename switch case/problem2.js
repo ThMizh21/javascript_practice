f62803a43d7c579dcdd894def266aee6afedd483
ThMizh21 <muthamizhanboobalan@gmail.com>
2024-09-05 17:40:12 +0530

    add new problem files to switch case

---
+++ switch case/problem2.js
@@ -0,0 +1,22 @@
+let character = "p";
+
+switch (character) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+    case "A":
+    case "E":
+    case "I":
+    case "O":
+    case "U":
+        console.log("Vowel");
+        break;
+
+    default:
+        console.log("It's a consonant");
+        break;
+
+
+}
