2ac4f83d2d19eb72473300fdf59e2af4b73def6b
ThMizh21 <muthamizhanboobalan@gmail.com>
2024-09-11 14:22:48 +0530

    add a file

---
+++ string/ex1.js
@@ -0,0 +1,7 @@
+// printing string in proper format
+let greet="hello";
+let name1="ThMizh";
+let content="Have a good look at the error occured";
+console.log(greet);
+console.log(name1);
+console.log(content);
---
+++ string/ex3.js
@@ -0,0 +1,6 @@
+// printing a special character in js 
+let spl="\"ThMizh\"";
+console.log(spl);
+
+let path="c:\\Thamizh_FSSA\\javascript_practice";
+console.log(path);
