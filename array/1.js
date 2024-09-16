851011b695bdb04f4665aaf8caabb0d04f66a5ab
ThMizh21 <muthamizhanboobalan@gmail.com>
2024-09-16 17:12:24 +0530

    new file

---
+++ array/1.js
@@ -0,0 +1,8 @@
+let students=["Sudhan","Hari","Rajesh","sankar","chukkun"];
+console.log("Tallest among then is "+students[0]);
+lastLen = students.length;
+console.log(lastLen)
+
+for(i=lastLen-1;i>=0;i--){
+    console.log(students[i]);
+}
---
+++ array/16-09-2024.js
@@ -0,0 +1,8 @@
+let car=["Audi","BMW","Cooper"];
+
+console.log("The value of car in index 3 is "+car[car.length-2]);
+console.log("");
+
+for (i=0;i<car.length;i++){
+    console.log(car[i]);
+}
