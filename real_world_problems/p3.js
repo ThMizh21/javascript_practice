0eea4d5f02cb90a3191102180c00f3489f8d9e07
ThMizh21 <muthamizhanboobalan@gmail.com>
2024-09-17 10:30:06 +0530

    adding a new and modikfied file

--- real_world_problems/p3.js
+++ real_world_problems/p3.js
@@ -2,9 +2,55 @@
 //  Use a switch statement to make the robot move in the direction entered by the user and
 //  print the robot's new position after each movement.
 
-const readline = Require('node.readline');
-const rl=readline.createinterface({
-    input:Process.stdin,
-    output:process.stdout,
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+
+let robotposition = {
+    x: 0,
+    y: 0
+};
+
+function robotMove(direction) {
+    switch (direction) {
+        case "north":
+            robotposition.x++;
+            console.log(`the robot moved north by ${robotposition.x},${robotposition.y}`);
+            break;
+            
+
+            case "south":
+                robotposition.x--;
+                console.log(`the robot moved south by ${robotposition.x},${robotposition.y}`);
+                break;
+
+                
+                case "east":
+            robotposition.y++;
+            console.log(`the robot moved east by ${robotposition.x},${robotposition.y}`);
+            break;
+
+
+        case "west":
+            robotposition.y--;
+            console.log(`the robot moved west by ${robotposition.x},${robotposition.y}`);
+            break;
+
+            default:
+            console.log("Invalid direction");
+            break;
+
+    }
+}
+
+
+
+rl.question("Enter the direction:", (input) => {
+    robotMove(input.trim().toLowerCase());
+    rl.close();
 });
 
---
+++ real_world_problems/p6.js
@@ -0,0 +1,6 @@
+let n=10;
+let altitude =0;
+for(i=1;i<=n;i++){
+    altitude+=100;
+ console.log( "The altitude is "+altitude+"ft");
+}
