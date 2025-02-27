031b0d477d0e9b794d3de27108af9c2c01f4161d
Muthamizhan <muthamizhanboobalan@gmail.com>
2025-02-27 09:18:15 +0530

    dsa completed

--- DSA/recursive.js
+++ DSA/recursive.js
@@ -76,3 +76,18 @@ function printPattern(n, current = 1, row = "") {
 }
 
 printPattern(5);
+
+// function factorial(n){
+//     if(n<=0){
+//         return -1
+//     }
+//     else if(n===1){
+//         return n
+//     }
+//     else{
+//         return n * factorial(n-1)
+//     }
+// }
+
+// let result = factorial(5);
+// console.log(result)
